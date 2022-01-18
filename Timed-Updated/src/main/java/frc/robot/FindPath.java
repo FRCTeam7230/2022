01addfeb8fad813477ded840f8ecbc30de20ad66
fightingj305 <ttohya305@gmail.com>
2022-01-18 11:19:56 -0800

    first 2022 update
    updated to 2022, removed some deprecated code, fixed issue with build

---
+++ Timed-Updated/src/main/java/frc/robot/FindPath.java
@@ -0,0 +1,93 @@
+package frc.robot;
+import edu.wpi.first.wpilibj.Filesystem;
+import java.nio.file.*;
+import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.Joystick;
+// import edu.wpi.first.wpilibj.TimedRobot;
+// import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.math.trajectory.TrajectoryUtil;
+// import edu.wpi.first.wpilibj.XboxController;
+// import edu.wpi.first.math.controller.PIDController;
+// import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.trajectory.Trajectory;
+// import edu.wpi.first.math.trajectory.Trajectory.State;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import java.util.List;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+// import frc.robot.Constants.OIConstants;
+// import frc.robot.subsystems.DriveSubsystem;
+// import frc.robot.Constants;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
+import java.io.*;
+//import java.util.List;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+//import edu.wpi.first.wpilibj.Timer;
+// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import edu.wpi.first.math.trajectory.TrajectoryUtil;
+// import edu.wpi.first.math.trajectory.Trajectory;
+// import edu.wpi.first.wpilibj.motorcontrol.Spark;
+//import frc.robot.RobotContainer;
+// import frc.robot.subsystems.DriveSubsystem;
+// import frc.robot.Mechanism;
+
+
+public class FindPath {
+
+Trajectory trajectory;
+
+String trajectoryJSON = "/home/lvuser/deploy/pjhvaths/Unnamed.wpilib.json";
+
+DifferentialDriveVoltageConstraint autoVoltageConstraint =
+        new DifferentialDriveVoltageConstraint(
+            new SimpleMotorFeedforward(DriveConstants.ksVolts,
+                                       DriveConstants.kvVoltSecondsPerMeter,
+                                       DriveConstants.kaVoltSecondsSquaredPerMeter),
+            DriveConstants.kDriveKinematics,
+            10);
+
+TrajectoryConfig config =
+        new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+                             AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+            // Add kinematics to ensure max speed is actually obeyed
+            .setKinematics(DriveConstants.kDriveKinematics)
+            // Apply the voltage constraint
+            .addConstraint(autoVoltageConstraint);
+
+public FindPath(){
+    this.FindPath();
+}
+public void FindPath(){
+try{
+    Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+    trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+        }catch (IOException e){
+            DriverStation.reportError("Can't read json", true);
+            trajectory = TrajectoryGenerator.generateTrajectory(
+        // Start at the origin facing the +X direction
+        new Pose2d(0, 0, new Rotation2d(0)),
+        // Pass through these two interior waypoints, making an 's' curve path
+        List.of(
+            new Translation2d(1, 1),
+            new Translation2d(2, -1)
+        ),
+        // End 3 meters straight ahead of where we started, facing forward
+        new Pose2d(3, 0, new Rotation2d(0)),
+        // Pass config
+        config
+    );
+  }
+}
+public Trajectory getPath(){
+ return trajectory;
+}
+}
