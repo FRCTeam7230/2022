68c1dee411428a5d310cb17afa5d342a2dd464c2
Rorago <willyuyang@gmail.com>
2022-10-29 13:03:14 -0700

    New Joystick Controls
    Makes the robot go at a quadratic rate of change and slowly ramps up depending on the x/y value of the joystick. Also cleans up the code and adds comments.

--- Timed-Updated/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Timed-Updated/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -17,59 +17,60 @@ public class Drivetrain {
     }
     public void drive(boolean tank, boolean driveModified){
         final double deadZone=0.4;
-        final double minZone=0.07;
-        final double xOffset = 0.0;
         //positive xOffset goes right
         //gets joystick values and creates curves
         double y = m_stick.getRawAxis(1);
-        double yprime = Math.pow(y,3);
-        // double yprime=-y;
+        // double y = Math.pow(y,3);
+        // double y=-y;
         double x = m_stick.getRawAxis(2);
-        double xprime = Math.pow(x,3);
-        // double xprime=x;
-        double accelFactor = 2.0;
-        double slowFactor = 0.85;
-        double speedFactor = 0.825;
-        double turnFactor = 0.925;
-        //The % power used
-        final double turnLimit = 0.4;
-        double speedLimit=0.5;
-        //Reports joystick numbers
-        // DriverStation.reportWarning("Raw Y,X: "+((Double)yprime).toString()+","+((Double)xprime).toString(),true);
-        //Mathmomagic! For X and Y 
-        if(minZone<Math.abs(yprime)){
-        // yprime=deadZone*Math.signum(yprime);
-            yprime=(Math.abs(yprime)/yprime)*(deadZone+speedLimit*(1-deadZone)*(Math.abs(yprime)+deadZone)/(1))-deadZone/4;
-            // DriverStation.reportWarning("BANANA"+((Double)Math.abs(yprime)).toString(),true);
+        // double x = Math.pow(x,3);
+        // double x=x;
+        final double zoomFactor = 2.0;
+        final double slowFactor = 0.85;
+        final double speedFactor = 0.825;
+        final double turnFactor = 0.925;
+
+        double speedY = 0.0;
+        double rateOfSpeedYChange = 0.0;
+        double speedX = 0.0;
+        double rateOfSpeedXChange = 0.0;
+        final double accel = 0.05;
+        if(deadZone < y && y > speedY) {
+            speedY += rateOfSpeedYChange;
+            rateOfSpeedYChange += accel;
+            //Quadratic Rate of Change if I think
+            //y goes forward and back
+            //replace ys below this with speed?
         }
-        else{
-        yprime=0;
+        else if (deadZone>=y){
+            speedY=0;
         }
-        if(minZone<Math.abs(xprime)){
-        xprime=Math.abs(xprime)/xprime*(deadZone+turnLimit*(1-deadZone)*(Math.abs(x)-0.1)/0.9);
-        // if (yprime<0){
-        //   yprime*=leftAdj;
-        // }
-            //DriverStation.reportWarning("KIWI"+((Double)Math.abs(xprime)).toString(),true);
+        if(deadZone < x && x > speedX) {
+            speedX += rateOfSpeedXChange;
+            rateOfSpeedXChange += accel;
+            //Quadratic Rate of Change if I think
+            //y goes forward and back
+            //replace ys below this with speed?
         }
-        else{
-        xprime=0;
+        else if (deadZone>=x){
+            speedX=0;
         }
-        // DriverStation.reportWarning("New Y,X: "+((Double)yprime).toString()+","+((Double)xprime).toString(),true);
+
+        // DriverStation.reportWarning("New Y,X: "+((Double)y).toString()+","+((Double)x).toString(),true);
         // R Bumper is 6
         if(m_stick.getRawButton(5)){
-        yprime*=accelFactor;
+        y*=zoomFactor;
         }
         
         if(m_stick.getRawButton(6)){
-        yprime*=slowFactor;
-        xprime*=slowFactor; 
+        y*=slowFactor;
+        x*=slowFactor; 
         }
-        xprime *= turnFactor;
-        yprime *= speedFactor;
+        x *= turnFactor;
+        y *= speedFactor;
         //Actual drive part
         if (!tank && !driveModified){
-            m_robotDrive.arcadeDrive(invertAxis * (xprime+xOffset), invertAxis *(yprime));
+            m_robotDrive.arcadeDrive(invertAxis * (speedX), invertAxis *(speedY));
         }
         if (m_stick.getRawButton(10)){
             m_robotDrive.resetEncoders();
